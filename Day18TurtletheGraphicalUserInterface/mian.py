17574b91c31d44d0b9346a0526ed56c443a0cb4c
billyxu008 <billyxu008@gmail.com>
2022-04-04 22:06:51 -0400

    Day18

---
+++ Day18TurtletheGraphicalUserInterface/mian.py
@@ -0,0 +1,29 @@
+from turtle import Screen, Turtle
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("black")
+
+
+# for n in range(3, 9):
+#     for x in range(0, n):
+#         timmy.forward(100)
+#         timmy.right(360 / n)
+
+
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for _ in range(num_sides):
+#         timmy.forward(100)
+#         timmy.right(angle)
+
+
+# for shape_side_n in range(3, 11):
+#     draw_shape(shape_side_n)
+
+
+# Draw a Random Walk
+
+
+screen = Screen()
+screen.exitonclick()
